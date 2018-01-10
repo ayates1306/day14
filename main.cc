3735aff806f79309477b4173dfc9313be5f54e6a
Alasdair Yates <ali1306@gmail.com>
2018-01-10 22:34:12 +0000

    further inheritance tests; but now there is an ambiguity with 2 base classes havng the same method

--- main.cc
+++ main.cc
@@ -6,32 +6,66 @@
 using std::cout;
 using std::cin;
 
+enum COLOUR {white, brown, black};
+
 class Horse {
 public:
-  Horse() { cout << "Horse constructor\n"; }
+  Horse() { cout << "Horse constructor "; itsAge = 1;}
+  Horse(int);
   virtual ~Horse() { cout << "Horse destructor "; }
   virtual void Whinny() const { cout << "Whinny!!\n"; }
+  virtual int GetItsAge() const { return itsAge;}
+  virtual COLOUR GetItsColour() const { return itsColour; }
 private:
   int itsAge;
+  COLOUR itsColour;
 };
+
+Horse::Horse(int a):
+  itsAge(a)
+{
+  cout << "Horse(int) constructor\n";
+}
 
 class Bird {
 public:
-  Bird() { cout << "Bird constructor\n";}
+  Bird() { cout << "Bird constructor "; itsWeight = 1; }
+  Bird(int);
   virtual ~Bird() { cout << "Bird destructor ";}
   virtual void Chirp() const { cout << "Chirp!!\n";}
   virtual void Fly() const { cout << "I can fly!\n";}
+  virtual int GetItsWeight() const { return itsWeight; }
+  virtual COLOUR GetItsColour() const { return itsColour; }
 private:
   int itsWeight;
+  COLOUR itsColour;
 };
+
+Bird::Bird(int w):
+  itsWeight(w)
+{
+  cout << "Bird(int) constructor\n";
+}
+
 
 class Pegasus : public Horse , public Bird
 {
 public:
   void Chirp() const { Whinny(); }
-  Pegasus() { cout << "Pegasus constructor\n";}
+  Pegasus(int magic_rank, int age, int weight);
   ~Pegasus() { cout << "Pegasus destructor ";}
+  int GetItsMagicRanking() const { return magic_ranking; }
+private:
+  int magic_ranking;
 };
+
+Pegasus::Pegasus(int magic_rank, int age, int weight):
+  Horse(age),
+  Bird(weight),
+  magic_ranking(magic_rank)
+{
+  cout << "Pegasus constructor\n";
+}
 
 const int MagicNumber =2;
 
@@ -39,17 +73,52 @@ int main(void)
 {
   Horse *pHorse1, *pHorse2;
   Bird *pBird1, *pBird2;
+  Pegasus *pPegasus;
   
+  cout << "Create a horse\n";
   pHorse1 = new Horse;
-  pHorse2 = new Pegasus;
+  cout << "\n";
+  cout << "Create a pegasus in a horse ptr\n";
+  pHorse2 = new Pegasus(7,8,9);
+  cout << "\n";
+  cout << "Create a bird\n";
   pBird1 = new Bird;
-  pBird2 = new Pegasus;
+  cout << "\n";
+  cout << "Create a pegasus in a bird ptr\n";
+  pBird2 = new Pegasus(1,2,3);
+  cout << "\n";
 
+  cout << "Now create a pegasus in a pegasus ptr\n";
+  pPegasus = new Pegasus(4,5,6);
+  cout << "\n";
+
+  cout << "pHorse1 whinny: ";
   pHorse1->Whinny();
+  cout << "Age: " << pHorse1->GetItsAge() << "\n";
+  cout << "pHorse2 whinny: ";
   pHorse2->Whinny();
+  cout << "Age: " << pHorse2->GetItsAge() << "\n";
+  cout << "Horse2 colour " << pHorse2->GetItsColour() << "\n";
 
+  cout << "pBird 1 chirp: ";
   pBird1->Chirp();
+  cout << "its weight " << pBird1->GetItsWeight() << "\n";
+  cout << "pBird 1 chirp: ";
   pBird2->Chirp();
+  cout << "its weight " << pBird1->GetItsWeight() << "\n";
+  cout << "Bird1 colour " << pBird1->GetItsColour() << "\n";
+
+  cout << "Pegasus class methods:\n";
+  pPegasus->Whinny();
+  pPegasus->Chirp();
+  pPegasus->Fly();
+  cout << "its magic " << pPegasus->GetItsMagicRanking() << "\n";
+  cout << "its age " << pPegasus->GetItsAge() << "\n";
+  cout << "its weight " << pPegasus->GetItsWeight() << "\n";
+  // GetItsColour() is ambiguous since both base classes
+  // has the same method
+  // cout << "its colour " << pPegasus->GetItsColour() << "\n";
+
 
   delete pHorse1;
   cout << "\n";
@@ -58,6 +127,8 @@ int main(void)
   delete pBird1;
   cout << "\n";
   delete pBird2;
+  cout << "\n";
+  delete pPegasus;
   cout << "\n";
 }
 
